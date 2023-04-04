5b19f5033cfcdbc7764b130d0f2d446f0af68c8e
Kimossab <kimossab@outlook.pt>
2023-04-04 20:07:22 +0100

    feat: remove histoire

--- histoire.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "histoire";
-import { HstVue } from "@histoire/plugin-vue";
-
-export default defineConfig({
-  setupFile: "/histoire.setup.ts",
-  plugins: [HstVue()],
-});
