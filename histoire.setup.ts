38183c4dddb0773f78e9b3bd0ae8dc984bac720e
Kimossab <kimossab@outlook.pt>
2023-04-04 20:07:22 +0100

    feat: remove histoire

--- histoire.setup.ts
+++
@@ -1,8 +0,0 @@
-import { createPinia } from "pinia";
-import { defineSetupVue3 } from "@histoire/plugin-vue";
-import "./src/assets/css/base.css";
-
-export const setupVue3 = defineSetupVue3(({ app, story, variant }) => {
-  const pinia = createPinia();
-  app.use(pinia); // Add Pinia store
-});
